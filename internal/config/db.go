590c420b131376082de1d8389bf97c878edb29e7
Fatma Janna <46950501+fatjan@users.noreply.github.com>
2025-01-25 20:47:00 +0700

    remove load test db

--- internal/config/db.go
+++ internal/config/db.go
@@ -21,14 +21,3 @@ func loadDatabaseConfig() db {
 		SSL:      os.Getenv("DB_SSL_MODE"),
 	}
 }
-
-func loadDatabaseTestConfig() db {
-	return db{
-		Host:     os.Getenv("TEST_DB_HOST"),
-		Port:     os.Getenv("TEST_DB_PORT"),
-		User:     os.Getenv("TEST_DB_USER"),
-		Password: os.Getenv("TEST_DB_PASSWORD"),
-		Database: os.Getenv("TEST_DB_NAME"),
-		SSL:      os.Getenv("TEST_DB_SSL_MODE"),
-	}
-}
